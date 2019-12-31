d8a6dc0109581b1575bb12572a3ec90820bc488b
Kimberly Conkle <kimberlyconkle88@gmail.com>
2019-12-31 00:37:53 +0000

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
-=======
   devise_for :installs
->>>>>>> fdf9aaca3111ae522026b197cf4f414cf012c658
   root "grams#index"
   resources :grams, only: [:new, :create]
 end
