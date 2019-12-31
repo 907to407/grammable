312eecb10a8362013d6fc45eb7bd577d924db1f9
Kimberly Conkle <kimberlyconkle88@gmail.com>
2019-12-31 17:07:29 +0000

    lesson 12 commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :installs
   root "grams#index"
-  resources :grams, only: [:new, :create]
+  resources :grams, only: [:new, :create, :show]
 end
